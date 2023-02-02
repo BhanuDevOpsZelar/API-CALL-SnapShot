ea5970194aa1ec719c61155d1b3a5569a4997e67
BhanuVardhan <bhanuvardhan@zelarsoft.com>
2023-02-02 17:39:53 +0530

    filtering data through input field and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Category from './Category';
+import Item from './Item'
 
 function App() {
   return (
     <div className="App">
-      <Category />
+<Category />
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Categories from './Categories';
+import { useState } from 'react';
+
+const List = ({Categories}) => {
+
+    const [value , setValue]=useState("");
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default List
